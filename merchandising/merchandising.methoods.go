f17e1269f57b21f2b47492876e030ae9d941b6f7
Saeed Heidari <saeed.heidari86@gmail.com>
2021-01-01 22:07:53 +0330

    merchandising update

--- merchandising/merchandising.methoods.go
+++ merchandising/merchandising.methoods.go
@@ -2,13 +2,6 @@ package merchandising
 
 import "encoding/json"
 
-func (t *RequestExist) Unmarshal(entry interface{}) RequestExist {
-	obj, _ := json.Marshal(entry)
-	response := RequestExist{}
-	_ = json.Unmarshal(obj, &response)
-	return response
-}
-
 func (t *RequestCreate) Unmarshal(entry interface{}) RequestCreate {
 	obj, _ := json.Marshal(entry)
 	response := RequestCreate{}
@@ -19,13 +12,6 @@ func (t *RequestCreate) Unmarshal(entry interface{}) RequestCreate {
 func (t *ResponseDetails) Unmarshal(entry interface{}) ResponseDetails {
 	obj, _ := json.Marshal(entry)
 	response := ResponseDetails{}
-	_ = json.Unmarshal(obj, &response)
-	return response
-}
-
-func (t *ResponseFindByIds) Unmarshal(entry interface{}) ResponseFindByIds {
-	obj, _ := json.Marshal(entry)
-	response := ResponseFindByIds{}
 	_ = json.Unmarshal(obj, &response)
 	return response
 }
