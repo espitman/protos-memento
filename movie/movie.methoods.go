148a413c6cdb5fe27091cca65d4eb7928ed6b69e
Saeed Heidari <saeed.heidari86@gmail.com>
2021-01-02 23:45:47 +0330

    movie update

--- movie/movie.methoods.go
+++ movie/movie.methoods.go
@@ -2,13 +2,6 @@ package merchandising
 
 import "encoding/json"
 
-func (t *RequestCreate) Unmarshal(entry interface{}) RequestCreate {
-	obj, _ := json.Marshal(entry)
-	response := RequestCreate{}
-	_ = json.Unmarshal(obj, &response)
-	return response
-}
-
 func (t *ResponseDetails) Unmarshal(entry interface{}) ResponseDetails {
 	obj, _ := json.Marshal(entry)
 	response := ResponseDetails{}
