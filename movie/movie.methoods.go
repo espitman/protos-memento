662e242fc53507c647807765f37eb44747257b4f
Saeed Heidari <saeed.heidari86@gmail.com>
2021-01-05 00:52:01 +0330

    movie update

--- movie/movie.methoods.go
+++ movie/movie.methoods.go
@@ -2,6 +2,13 @@ package movie
 
 import "encoding/json"
 
+func (t *TMDBDetails) Unmarshal(entry interface{}) TMDBDetails {
+	obj, _ := json.Marshal(entry)
+	response := TMDBDetails{}
+	_ = json.Unmarshal(obj, &response)
+	return response
+}
+
 func (t *ResponseDetails) Unmarshal(entry interface{}) ResponseDetails {
 	obj, _ := json.Marshal(entry)
 	response := ResponseDetails{}
